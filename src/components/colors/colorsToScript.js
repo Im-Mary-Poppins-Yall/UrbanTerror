7a0e1c99ac4c8de7753f622ae78ac5a300dbc95b
Im-Mary-Poppins-Yall <elgiantpopcorn@gmail.com>
2021-05-12 17:25:26 +0200

    :bug: Fix colors script generator

--- src/components/colors/colorsToScript.js
+++ src/components/colors/colorsToScript.js
@@ -2,14 +2,13 @@ import { SITE_URL } from '../../constants.js';
 import { COLOR_PRESET_NAMES, CROSSHAIR_COLORS, TACS_COLORS, TEAM_COLORS } from './constants.js';
 
 const CYCLE_COMMAND_NAME = 'cycle_color_preset';
+const COMMAND_PREFIX = 'color_preset_';
 
 const presetToScriptLine = (preset) => {
-  const commandPrefix = 'color_preset_';
-
   const setEnemiesColor = `set cg_skinEnemy ""${TEAM_COLORS[ preset.enemies ].code}""`;
   const setCrosshairColor = `set cg_crosshairrgb ""${CROSSHAIR_COLORS[ preset.crosshair ].code}""`;
   const setTacsColor = `set cg_nvg ""${TACS_COLORS[ preset.tacs ].code}""`;
-  const setNextCommand = `set ${CYCLE_COMMAND_NAME} vstr ${commandPrefix}${preset.nextCommandName}`;
+  const setNextCommand = `set ${CYCLE_COMMAND_NAME} vstr ${COMMAND_PREFIX}${preset.nextCommandName}`;
 
   const fullCommand = [
     setEnemiesColor,
@@ -18,7 +17,7 @@ const presetToScriptLine = (preset) => {
     setNextCommand,
   ].join('; ');
 
-  return `set ${commandPrefix}${preset.commandName} "${fullCommand}"`;
+  return `set ${COMMAND_PREFIX}${preset.commandName} "${fullCommand}"`;
 };
 
 const presetsToScriptLines = (presets) => {
@@ -45,6 +44,8 @@ export function colorsToScript (subscriptionColorPresets) {
 echo "Loading colors";
 
 ${presetsToScriptLines(indexedPresets)}
+
+set ${CYCLE_COMMAND_NAME} "vstr ${COMMAND_PREFIX}${COLOR_PRESET_NAMES[ 0 ]}"
 
 bind ${cycle} "vstr ${CYCLE_COMMAND_NAME}"
 
